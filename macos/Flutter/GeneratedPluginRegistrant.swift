4623d4e83de4545aecc66bd801c9fd7bc7b7ff02
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2024-07-26 19:07:49 +0600

    change in build.gradle

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayers_darwin
 import geolocator_apple
 import path_provider_foundation
-import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
