19486725537e69cc703f00887e2c4571708bf79c
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2024-07-28 22:47:35 +0600

    give permission

--- lib/screens/home_page/home_page.dart
+++ lib/screens/home_page/home_page.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weatherapp/controllers/weather_controller.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final WeatherController weatherController = Get.put(WeatherController());
     return Scaffold(
       appBar: AppBar(
         title: const Text('weather app'),
       ),
-      body: const SingleChildScrollView( 
-        child: Column( 
-          children: [
-
-          ],
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [],
         ),
       ),
     );
